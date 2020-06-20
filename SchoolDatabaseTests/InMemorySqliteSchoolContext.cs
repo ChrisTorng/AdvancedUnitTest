c73d90becaa69651c78ce6873b354597bf76a2c9
ChrisTorng <christorng@hotmail.com>
2020-06-20 14:51:39 +0800

    Extracts SchoolContext related from StudentRepositoryInMemorySqliteTests to newly InMemorySqliteSchoolContext.

---
+++ SchoolDatabaseTests/InMemorySqliteSchoolContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolDatabase.Tests
+{
+    internal class InMemorySqliteSchoolContext : ISchoolDatabase
+    {
+        private readonly DbConnection connection;
+        private readonly SchoolContext schoolContext;
+        private bool disposed = false;
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        public InMemorySqliteSchoolContext()
+        {
+            this.connection = CreateInMemoryDatabase();
+            this.schoolContext = new SchoolContext(new DbContextOptionsBuilder<SchoolContext>()
+                .UseSqlite(this.connection)
+                .Options);
+
+            this.schoolContext.Database.EnsureCreated();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.schoolContext.Dispose();
+                this.connection.Dispose();
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IQueryable<Student> Students =>
+            this.schoolContext.Students;
+    }
+}
--- SchoolDatabaseTests/StudentRepositoryInMemorySqliteTests.cs
+++ SchoolDatabaseTests/StudentRepositoryInMemorySqliteTests.cs
@@ -1,58 +1,17 @@
-﻿using System.Data.Common;
-using System.Linq;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SchoolDatabase.Tests
 {
     [TestClass]
-#pragma warning disable CA1001 // Types that own disposable fields should be disposable
     public class StudentRepositoryInMemorySqliteTests
-#pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
-        private DbConnection connection;
-        private SchoolContext schoolContext;
-
-        private static DbConnection CreateInMemoryDatabase()
-        {
-            var connection = new SqliteConnection("Filename=:memory:");
-
-            connection.Open();
-
-            return connection;
-        }
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            this.connection = CreateInMemoryDatabase();
-            this.schoolContext = new SchoolContext(new DbContextOptionsBuilder<SchoolContext>()
-                .UseSqlite(this.connection)
-                .Options);
-
-            this.schoolContext.Database.EnsureCreated();
-        }
-
-        [TestCleanup]
-        public void TestCleanup()
-        {
-            if (this.schoolContext != null)
-            {
-                this.schoolContext.Dispose();
-            }
-
-            if (this.connection != null)
-            {
-                this.connection.Dispose();
-            }
-        }
-
         [TestMethod]
-        public void StudentRepository_Empty_Test()
+        public void StudentRepository_EmptyAllStudents_Test()
         {
-            var studentRepository = new StudentRepository(this.schoolContext);
+            using var schoolContext = new InMemorySqliteSchoolContext();
+            var studentRepository = new StudentRepository(schoolContext);
 
             Assert.IsFalse(studentRepository.AllStudents.Any());
         }
