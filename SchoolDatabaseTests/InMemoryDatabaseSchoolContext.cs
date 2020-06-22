4d911f5d03752226dde25c10977185ab79f8a250
ChrisTorng <christorng@hotmail.com>
2020-06-22 16:12:39 +0800

    InMemoryDatabaseSchoolContext implements IBaseSchoolDatabase, copies BaseSchoolDatabase's codes. Deletes unused BaseSchoolDatabase.

--- SchoolDatabaseTests/InMemoryDatabaseSchoolContext.cs
+++ SchoolDatabaseTests/InMemoryDatabaseSchoolContext.cs
@@ -1,15 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace SchoolDatabase.Tests
 {
-    internal class InMemoryDatabaseSchoolContext : BaseSchoolDatabase
+    internal class InMemoryDatabaseSchoolContext : IBaseSchoolDatabase
     {
+        private readonly SchoolContext schoolContext;
+        private bool disposed = false;
+
+        public IQueryable<Student> Students =>
+            this.schoolContext.Students;
+
         public InMemoryDatabaseSchoolContext()
-            : base(new DbContextOptionsBuilder<SchoolContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options)
         {
+            this.schoolContext = new SchoolContext(new DbContextOptionsBuilder<SchoolContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+
+            this.schoolContext.Database.EnsureCreated();
+        }
+
+        public void AddStudents(IEnumerable<Student> students)
+        {
+            this.schoolContext.AddRange(students);
+            this.schoolContext.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.schoolContext.Dispose();
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
