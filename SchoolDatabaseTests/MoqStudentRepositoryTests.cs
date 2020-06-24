2cf47b8e5b8b09bafbf52d0a4407d659e5ac35dd
ChrisTorng <christorng@hotmail.com>
2020-06-24 15:19:05 +0800

    SchoolDatabaseTests adds Moq package. Adds MoqStudentRepositoryTests.cs.

---
+++ SchoolDatabaseTests/MoqStudentRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace SchoolDatabase.Tests
+{
+    [TestClass]
+    public class MoqStudentRepositoryTests
+    {
+        [TestMethod]
+        public void StudentRepository_EmptyAllStudents_Test()
+        {
+            var schoolContextMock = new Mock<ISchoolDatabase>();
+
+            var studentRepository = new StudentRepository(schoolContextMock.Object);
+
+            Assert.IsFalse(studentRepository.AllStudents.Any());
+        }
+
+        [TestMethod]
+        public void StudentRepository_AllStudentsQuery_Test()
+        {
+            var schoolContextMock = new Mock<ISchoolDatabase>();
+            schoolContextMock.Setup(s => s.Students).Returns(new Student[]
+                {
+                    new Student
+                    {
+                        FirstMidName = "a",
+                        LastName = "b",
+                        EnrollmentDate = new DateTime(2, 2, 2),
+                    },
+                    new Student
+                    {
+                        FirstMidName = "c",
+                        LastName = "d",
+                        EnrollmentDate = new DateTime(3, 3, 3),
+                    },
+                }.AsQueryable());
+
+            var studentRepository = new StudentRepository(schoolContextMock.Object);
+            Assert.IsTrue(studentRepository.AllStudents.Any());
+            Assert.AreEqual(2, studentRepository.AllStudents.Count());
+
+            var student = studentRepository.AllStudents
+                .Where(s => s.FirstMidName == "c").Single();
+            Assert.AreEqual("d", student.LastName);
+            Assert.AreEqual(new DateTime(3, 3, 3), student.EnrollmentDate);
+        }
+
+        [TestMethod]
+        public void StudentRepository_CurrentStudentsStartDate_Test()
+        {
+            Assert.AreEqual(new DateTime(2017, 8, 1), StudentRepository.CurrentStudentsStartDate);
+        }
+    }
+}
