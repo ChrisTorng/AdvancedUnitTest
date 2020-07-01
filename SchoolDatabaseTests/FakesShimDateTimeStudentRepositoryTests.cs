32ed87b978d198374cbc2e2b7e8d35ac2f592aeb
ChrisTorng <christorng@hotmail.com>
2020-07-01 14:22:46 +0800

    Renames all fakes as fakes_. Comments out all Fakes related in SchoolDatabaseTests project. Comments out FakesShimDateTimeStudentRepositoryTests and FakesShimDefaultDateTimeStudentRepositoryTests.

--- SchoolDatabaseTests/FakesShimDateTimeStudentRepositoryTests.cs
+++ SchoolDatabaseTests/FakesShimDateTimeStudentRepositoryTests.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Fakes;
-using System.Linq;
-using Microsoft.QualityTools.Testing.Fakes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Fakes;
+////using System.Linq;
+////using Microsoft.QualityTools.Testing.Fakes;
+////using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SchoolDatabase.Tests
-{
-    [TestClass]
-    [TestCategory("SkipWhenLiveUnitTesting")]
-    public class FakesShimDateTimeStudentRepositoryTests
-    {
-        [TestMethod]
-        [DataRow(2020, 7, 31, 2017, 8, 1)]
-        [DataRow(2020, 8, 1, 2018, 8, 1)]
-        [DataRow(2021, 8, 1, 2019, 8, 1)]
-        public void StudentRepository_CurrentStudentsStartDate_Test(
-            int inputYear, int inputMonth, int inputDay,
-            int expectedYear, int expectedMonth, int expectedDay)
-        {
-            using var schoolContext = new MockSchoolDatabase();
+////namespace SchoolDatabase.Tests
+////{
+////    [TestClass]
+////    [TestCategory("SkipWhenLiveUnitTesting")]
+////    public class FakesShimDateTimeStudentRepositoryTests
+////    {
+////        [TestMethod]
+////        [DataRow(2020, 7, 31, 2017, 8, 1)]
+////        [DataRow(2020, 8, 1, 2018, 8, 1)]
+////        [DataRow(2021, 8, 1, 2019, 8, 1)]
+////        public void StudentRepository_CurrentStudentsStartDate_Test(
+////            int inputYear, int inputMonth, int inputDay,
+////            int expectedYear, int expectedMonth, int expectedDay)
+////        {
+////            using var schoolContext = new MockSchoolDatabase();
 
-            using var shim = ShimsContext.Create();
-            ShimDateTime.NowGet =
-                () => new DateTime(inputYear, inputMonth, inputDay);
+////            using var shim = ShimsContext.Create();
+////            ShimDateTime.NowGet =
+////                () => new DateTime(inputYear, inputMonth, inputDay);
 
-            var studentRepository = new StudentRepository(schoolContext);
-            Assert.AreEqual(new DateTime(expectedYear, expectedMonth, expectedDay),
-                studentRepository.CurrentStudentsStartDate);
-        }
+////            var studentRepository = new StudentRepository(schoolContext);
+////            Assert.AreEqual(new DateTime(expectedYear, expectedMonth, expectedDay),
+////                studentRepository.CurrentStudentsStartDate);
+////        }
 
-        [TestMethod]
-        [DynamicData(nameof(CurrentStudentsTestData), DynamicDataSourceType.Method)]
-        public void StudentRepository_CurrentStudents_Test(DateTime now, int expectedLength)
-        {
-            var allStudents = new Student[]
-            {
-                new Student
-                {
-                    FirstMidName = "a",
-                    LastName = "b",
-                    EnrollmentDate = new DateTime(2018, 8, 1),
-                },
-                new Student
-                {
-                    FirstMidName = "c",
-                    LastName = "d",
-                    EnrollmentDate = new DateTime(2018, 7, 31),
-                },
-            };
+////        [TestMethod]
+////        [DynamicData(nameof(CurrentStudentsTestData), DynamicDataSourceType.Method)]
+////        public void StudentRepository_CurrentStudents_Test(DateTime now, int expectedLength)
+////        {
+////            var allStudents = new Student[]
+////            {
+////                new Student
+////                {
+////                    FirstMidName = "a",
+////                    LastName = "b",
+////                    EnrollmentDate = new DateTime(2018, 8, 1),
+////                },
+////                new Student
+////                {
+////                    FirstMidName = "c",
+////                    LastName = "d",
+////                    EnrollmentDate = new DateTime(2018, 7, 31),
+////                },
+////            };
 
-            using var schoolContext = new MockSchoolDatabase(allStudents);
+////            using var schoolContext = new MockSchoolDatabase(allStudents);
 
-            using var shim = ShimsContext.Create();
-            ShimDateTime.NowGet = () => now;
+////            using var shim = ShimsContext.Create();
+////            ShimDateTime.NowGet = () => now;
 
-            var studentRepository = new StudentRepository(schoolContext);
-            CollectionAssert.AreEqual(allStudents.Take(expectedLength).ToArray(),
-                studentRepository.CurrentStudents.ToArray());
-        }
+////            var studentRepository = new StudentRepository(schoolContext);
+////            CollectionAssert.AreEqual(allStudents.Take(expectedLength).ToArray(),
+////                studentRepository.CurrentStudents.ToArray());
+////        }
 
-        private static IEnumerable<object[]> CurrentStudentsTestData()
-        {
-            yield return new object[] { new DateTime(2020, 7, 31), 2 };
-            yield return new object[] { new DateTime(2020, 8, 1), 1 };
-            yield return new object[] { new DateTime(2021, 8, 1), 0 };
-        }
-    }
-}
+////        private static IEnumerable<object[]> CurrentStudentsTestData()
+////        {
+////            yield return new object[] { new DateTime(2020, 7, 31), 2 };
+////            yield return new object[] { new DateTime(2020, 8, 1), 1 };
+////            yield return new object[] { new DateTime(2021, 8, 1), 0 };
+////        }
+////    }
+////}
