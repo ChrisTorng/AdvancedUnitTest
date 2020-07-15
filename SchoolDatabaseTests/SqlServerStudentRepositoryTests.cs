e3714da6ba5e73d5542382c33c4832cfc229694b
ChrisTorng <christorng@hotmail.com>
2020-07-15 10:33:26 +0800

    SchoolDatabaseTests project adds Microsoft.EntityFrameworkCore.SqlServer NuGet package. Add SqlServerSchoolContext and SqlServerStudentRepositoryTests.

---
+++ SchoolDatabaseTests/SqlServerStudentRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolDatabase.Tests
+{
+    [TestClass]
+    public class SqlServerStudentRepositoryTests
+    {
+        [TestMethod]
+        public void StudentRepository_EmptyAllStudents_Test()
+        {
+            using var schoolContext = new SqlServerSchoolContext();
+
+            var studentRepository = new StudentRepository(schoolContext);
+            Assert.IsFalse(studentRepository.AllStudents.Any());
+        }
+
+        [TestMethod]
+        public void StudentRepository_AllStudentsQuery_Test()
+        {
+            using var schoolContext = new SqlServerSchoolContext(new Student[]
+                {
+                    new Student
+                    {
+                        FirstMidName = "a",
+                        LastName = "b",
+                        EnrollmentDate = new DateTime(2, 2, 2),
+                    },
+                    new Student
+                    {
+                        FirstMidName = "c",
+                        LastName = "d",
+                        EnrollmentDate = new DateTime(3, 3, 3),
+                    },
+                });
+
+            var studentRepository = new StudentRepository(schoolContext);
+            Assert.IsTrue(studentRepository.AllStudents.Any());
+            Assert.AreEqual(2, studentRepository.AllStudents.Count());
+
+            var student = studentRepository.AllStudents
+                .Where(s => s.FirstMidName == "c").Single();
+            Assert.AreEqual("d", student.LastName);
+            Assert.AreEqual(new DateTime(3, 3, 3), student.EnrollmentDate);
+        }
+    }
+}
